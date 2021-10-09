b115afc5961b730543dde9432c4483b52f58c861
Laurent Michel <softozor@gmail.com>
2021-10-09 17:40:24 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace App
+{
+    using System.Threading.Tasks;
+    using Ninject;
+    using Softozor.Publishodon;
+    using Softozor.Publishodon.Logging;
+    using log4net;
+    using App.Properties;
+
+    internal class Program
+    {
+
+        private static readonly ILog Log = LoggingSetup.CreateLog(Resources.log4net, typeof(Program), nameof(Program));
+
+        private static async Task<int> Main(string[] args)
+        {
+            Log.Info("starting application");
+            System.Console.WriteLine("caca");
+            using var kernel = KernelFactory.Create();
+            var bootstrapper = kernel.Get<Bootstrapper>();
+            var result = await bootstrapper.RunAsync(args);
+            return (int) result;
+        }
+    }
+}
